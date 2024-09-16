bf475880aa69a37043e64c76d32e5eea87a0daa3
Steven Haliburton <haliburton7@gmail.com>
2024-09-16 14:48:50 -0500

    working toggle

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
 import "./ToggleSwitch.css";
 const ToggleSwitch = () => {
-  console.log("Toggle Switch");
-  const [currentTeperatureUnit, handelToggleSwitchChange] = useState("");
+  const [currentTemperatureUnit, handelToggleSwitchChange] = useState("C");
 
   const handleChange = (e) => {
-    console.log(e);
+    if (currentTemperatureUnit === "C") handelToggleSwitchChange("F");
+    if (currentTemperatureUnit === "F") handelToggleSwitchChange("C");
   };
+  console.log(currentTemperatureUnit);
   return (
     <label className="switch">
       <input type="checkbox" className="switch__box" onChange={handleChange} />
-      <span> </span>
-      <p>F</p>
-      <p>C</p>
+      <span
+        className={
+          currentTemperatureUnit === "F"
+            ? "switch__slider switch__slider-F"
+            : "switch__slider switch__slider-C"
+        }
+      >
+        {" "}
+      </span>
+      <p
+        className={`switch__temp-F ${
+          currentTemperatureUnit === "F" && "switch__active"
+        }`}
+      >
+        F
+      </p>
+      <p
+        className={`switch__temp-C ${
+          currentTemperatureUnit === "C" && "switch__active"
+        }`}
+      >
+        C
+      </p>
     </label>
   );
 };
