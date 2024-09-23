9a5f6a962a3d89e74ee61373a0383933deedcfde
Steven Haliburton <haliburton7@gmail.com>
2024-09-23 07:23:24 -0500

    sumbit 5

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,28 +1,26 @@
-import { defaultClothingItems } from "../../utils/constants.js";
 import "./ClothesSection.css";
-import ItemCard from "../ItemCard/ItemCard.jsx";
+import ItemCard from "../ItemCard/ItemCard";
 
 function ClothesSection({ handleAddClick, handleCardClick, clothingItems }) {
   return (
     <div className="clothes-section">
       <div className="clothes-section__header">
-        <p className="clothes-section__items-title">Your Items</p>
+        <p className="clothes-section__title">Your items</p>
         <button
           className="clothes-section__add-button"
           onClick={handleAddClick}
         >
-          + Add New
+          + Add new
         </button>
       </div>
-      <ul className="cards__list">
+      <ul className="clothes-section__cards-list">
         {clothingItems.map((item) => {
           return (
             <ItemCard
               key={item._id}
               item={item}
               onCardClick={handleCardClick}
-              clothingItems={clothingItems}
-            ></ItemCard>
+            />
           );
         })}
       </ul>
